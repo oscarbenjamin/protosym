956f70bd108aa009154a417f4c541d81fd3cdb0b
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-02-23 12:57:14 +0000

    Add coverage measurement to quicktest.sh

--- quicktest.sh
+++ quicktest.sh
@@ -2,5 +2,6 @@
 
 poetry run pre-commit run --all-files
 poetry run mypy src tests
-poetry run pytest
 poetry run python -m xdoctest --quiet protosym
+poetry run pytest --cov=protosym
+poetry run coverage html
