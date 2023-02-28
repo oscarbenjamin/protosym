a420fee210fbec496a5a2bc42486f58c787986ef
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-02-28 13:25:50 +0000

    style(core): Add pass to exception class

--- src/protosym/core/exceptions.py
+++ src/protosym/core/exceptions.py
@@ -4,6 +4,8 @@
 class ProtoSymError(Exception):
     """Superclass for all protosym exceptions."""
 
+    pass
+
 
 class NoEvaluationRuleError(ProtoSymError):
     """Raised when an :class:`Evaluator` has no rule for an expression."""
