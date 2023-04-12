4da0cfe2b9a1cadd9140dd497ab40db7bc27f3c2
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-04-12 14:32:57 +0100

    test: add --rs CLI arg to quicktest.sh

--- quicktest.sh
+++ quicktest.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# # Clone rust_protosym in parallel directory:
+#
+# git clone https://github.com/oscarbenjamin/rust_protosym.git ../rust_protosym
+
+if [ "$1" = "--rs" ]; then
+    # Rebuild and reinstall rust_protosym:
+    cd ../rust_protosym
+      maturin build -r
+      pip install target/wheels/*.whl  --force-reinstall
+    cd -
+fi
+
 poetry run pre-commit run --all-files
 poetry run mypy src tests
 poetry run python -m xdoctest --quiet protosym
