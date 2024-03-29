794a86a6f5c1c24ba79e3e66262b7e0a9fc3e9df
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2024-03-29 12:02:51 +0000

    Build docs and test Python 3.12 and 3.13

--- checks.sh
+++ checks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+set -o errexit
+
 pre-commit run --all-files
-mypy src tests
+mypy --python-version=3.12 src tests
+sphinx-build -b html docs docs/_build
 python -m xdoctest --quiet protosym
 pytest --cov=protosym
 coverage html
